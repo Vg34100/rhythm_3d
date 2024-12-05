44b2651de61c377e58f2557914b7761ed3e15b0f
Vg34100 <videogamer341x@gmail.com>
2024-12-04 20:37:07 -0800

    feat: remix mode implemented

--- src/Assignments/FinalProject.h
+++ src/Assignments/FinalProject.h
@@ -20,4 +20,40 @@ private:
     std::unique_ptr<Scene> currentScene;
     SceneType currentSceneType;
     double lastFrameChange;
+
+
+    // Remix mode data
+    struct RemixData {
+        bool enabled;
+        double timeInCurrentScene;
+        double minSceneTime;  // Minimum time before allowing scene switch
+        double maxSceneTime;  // Maximum time before forcing scene switch
+        std::vector<SceneType> availableScenes;
+
+        RemixData() : enabled(false),
+            timeInCurrentScene(0.0),
+            minSceneTime(25.0),  // 10 seconds minimum
+            maxSceneTime(50.0)   // 20 seconds maximum
+        {
+            // Initialize available scenes for remix
+            availableScenes = {
+                SceneType::TambourineScene,
+                SceneType::BuiltToScaleScene
+                // Can add more scenes here
+            };
+        }
+
+        SceneType getRandomScene(SceneType currentType) {
+            if (availableScenes.empty()) return currentType;
+
+            SceneType newType;
+            do {
+                int index = rand() % availableScenes.size();
+                newType = availableScenes[index];
+            } while (newType == currentType && availableScenes.size() > 1);
+
+            return newType;
+        }
+    } remixData;
+
 };
